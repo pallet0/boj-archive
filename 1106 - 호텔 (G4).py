c2fcca4b868f68bbf752ff42b03cf43ca3b87703
2021147585 <greenet503@yonsei.ac.kr>
2026-04-11 22:51:18 +0900

    1106 clear!

---
+++ 1106 - 호텔 (G4).py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+c, n = map(int, input().split())
+cities = [list(map(int, input().split())) for _ in range(n)]
+
+dp = [float('inf')] * (c+101) # dp[i] = 고객 i명을 얻는 데 필요한 최소 비용
+dp[1] = 0
+
+for cost, customer in cities:
+    for w in range(customer, c+101):
+        if dp[w-customer] + customer < dp[w]:
+            dp[w] = dp[w-customer] + cost
+
+print(min(dp[c:]))
